6524b3015c1ac80f2cfdce72d569bd6dbbb36cf4
mrjakeball <kuzmin_aleksey2004@mail.ru>
2026-09-17 17:14:44 +1000

    chore: initialize CineFlow UIKit project
    Add the initial UIKit and Storyboard project structure.
    Configure the app and verify it launches successfully in the iOS Simulator.

---
+++ CineFlow/ViewController.swift
@@ -0,0 +1,23 @@
+// xcode: set sdk=iOS
+
+//
+//  ViewController.swift
+//  CineFlow
+//
+//  Created by Aleshka on 16.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+    }
+
+
+}
+
